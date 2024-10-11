ddf8f97fe493791c191c8c0364835de91cad524c
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-10-11 06:34:23 +0300

    add edit file

--- EditArray/src/ArraY.js
+++ EditArray/src/ArraY.js
@@ -2,49 +2,72 @@ import { useState } from "react";
 
 let newID = 4;
 
-export default function ArraY(){
+export default function ArraY() {
+  const [Inputs, setInputs] = useState("");
 
-    const [Inputs,setInputs] = useState("");
+  const [element, setelement] = useState([
+    { id: 1, name: "phone" },
+    { id: 2, name: "pc" },
+    { id: 3, name: "laptop" },
+  ]);
+  const device = element.map((el) => {
+    return (
+      <li key={el.id}>
+        {el.name}
+        <button
+          onClick={() => {
+            deletbtn(el.id);
+          }}
+        >Delet
+        </button>
+        <button
+          onClick={() => {
+            Edittbtn(el.id);
+          }}
+        >Edit
+        </button>
+      </li>
+    );
+  });
 
-    const [element,setelement] = useState([{id:1, name:"phone"},{id:2, name:"pc"},{id:3, name:"laptop"}]);
-    const device= element.map((el)  =>{
-      return <li key={el.id}>{el.name}<button onClick={()=>{deletbtn(el.id)}}>Delet</button></li>
-      }); 
-
-  function handelAdd(){
-      setelement([...element,{id:newID,name:Inputs}]);
-      newID++;
-      setInputs("");
-    }
-  function deletbtn(id){
-    //This is the wrong way ×××××
-    // const newelement =[...element];
-    // newelement.splice(id-1,1);
-    // setelement(newelement);
-
-    const newelement =element.filter((el) => {
-      //This is the way to write ✓✓
-      // if(el.id === id)
-      // return false;
-      // return true;
-
-      // This is the best way ✓✓✓✓✓
-      return el.id !== id ;
+  function handelAdd() {
+    setelement([...element, { id: newID, name: Inputs }]);
+    newID++;
+    setInputs("");
+  }
+  function deletbtn(id) {
+    const newelement = element.filter((el) => {
+      return el.id !== id;
     });
     setelement(newelement);
-    
   }
-    return <>
-
+  function Edittbtn(id) {
+    const newelement = element.map((el) => {
+      if (el.id === id) {
+        let theElementEdited ={...element,name: el.name + " 23"}
+        return theElementEdited; 
+      }else{
+        return el;
+      }
+    });
+    setelement(newelement);
+  }
+  return (
+    <>
       <div className="">
-           {device}
-         <div>
-         <input type="text" 
-          value={Inputs} 
-          onChange={(e)=>{setInputs(e.target.value)}}/>
-          <hr/>
+        {device}
+        <div>
+          <input
+            type="text"
+            value={Inputs}
+            onChange={(e) => {
+              setInputs(e.target.value);
+            }}
+          />
+          <hr />
           <button onClick={handelAdd}>log in</button>
-          </div>
-     </div>
+        </div>
+      </div>
     </>
+  );
 }
