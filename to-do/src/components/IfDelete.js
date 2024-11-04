f4beb8ee8ce8e312ce725db8691e8792c5b0b59e
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-11-05 00:18:37 +0200

    edit in to-do

--- to-do/src/components/IfDelete.js
+++ to-do/src/components/IfDelete.js
@@ -2,7 +2,7 @@ export default function IfDeelet({ No, Yes }) {
     return (
       <div className="bg-[#0c16149c] w-full h-full absolute top-0 left-0 flex justify-center items-center text-white">
         <div className="border-solid border-2 border-red-700 w-[70%] h-[200px] rounded-md">
-          <h2 className="text-left text-2xl font-bold p-4 mt-7">Are You Sure To Delete This Task?</h2>
+          <h2 className="text-left text-2xl font-bold p-4 mt-7"> -Are You Sure To Delete This Task ?</h2>
           <button className="Delet" onClick={No}>No</button>
           <button className="Delet" onClick={Yes}>Yes</button>
         </div>
--- to-do/src/test.js
+++ to-do/src/test.js
@@ -1,71 +1,137 @@
 import { useState } from "react";
 import IfDeelet from "./components/IfDelete";
-let NewId =1;
+let NewId = 1;
+
 export default function Test() {
   const [input, setinput] = useState("");
-  const [elements, setelements] = useState([{ id: 0, content: "" }]);
+  const [elements, setelements] = useState([
+    { id: 0, content: "", isEditing: false, EditIcone: true },
+  ]);
   const [Deelet, setDeelet] = useState("");
-
 
   const device = elements.map((el) => {
     return (
-      <li key={el.id}>
-        {el.content}
-        <i className="fa-solid fa-pencil" onClick={()=>HandelEdit(el.id)}></i>
-        <i className="fa-solid fa-trash" onClick={()=>HandelDelete(el.id)}></i>
-      </li>
+      <div className=" flex justify-between p-2 my-3 border-dashed border-2 border-gray-500 rounded-md">
+        <div>
+          <li key={el.id}>
+            {el.isEditing ? (
+              <input
+                type="text"
+                value={el.content}
+                onChange={(e) => handleInputChange(el.id, e.target.value)}
+                autoFocus
+              />
+            ) : (
+              <>{el.content}</>
+            )}
+          </li>
+        </div>
+
+        <div>
+          {el.EditIcone ? (
+            <i
+              className="fa-solid fa-pencil"
+              onClick={() => HandelEdit(el.id)}
+            ></i>
+          ) : (
+            <i
+              class="fa-regular fa-circle-check"
+              onClick={() => handleSave(el.id)}
+            ></i>
+          )}
+          <i
+            className="fa-solid fa-trash"
+            onClick={() => HandelDelete(el.id)}
+          ></i>
+        </div>
+      </div>
     );
   });
 
   function HandelAdd() {
-    setelements([...elements, { id: NewId , content: input }]);
+    setelements([
+      ...elements,
+      { id: NewId, content: input, isEditing: false, EditIcone: true },
+    ]);
     NewId++;
     setinput("");
   }
-  function HandelEdit(id){
 
-  }
-  function HandelDelete(id){
-    setDeelet(<IfDeelet Yes={()=>HandelYes(id)} No={HandelNo}/>)
-  }
-  function HandelYes(id){
-    const NewElement=elements.filter((e) =>{
-      return (e.id !== id)
+  function HandelEdit(id) {
+    const updatedElements = elements.map((el) => {
+      if (el.id === id) {
+        return { ...el, isEditing: true, EditIcone: false };
+      } else {
+        return el;
+      }
     });
+    setelements(updatedElements);
+  }
+
+  function handleInputChange(id, newValue) {
+    const updatedElements = elements.map((el) => {
+      if (el.id === id) {
+        return { ...el, content: newValue };
+      } else {
+        return el;
+      }
+    });
+    setelements(updatedElements);
+  }
+
+  function handleSave(id) {
+    const updatedElements = elements.map((el) => {
+      if (el.id === id) {
+        return { ...el, isEditing: false, EditIcone: true };
+      } else {
+        return el;
+      }
+    });
+    setelements(updatedElements);
+  }
+
+  function HandelDelete(id) {
+    setDeelet(<IfDeelet Yes={() => HandelYes(id)} No={HandelNo} />);
+  }
+
+  function HandelYes(id) {
+    const NewElement = elements.filter((e) => e.id !== id);
     setelements(NewElement);
     HandelNo();
   }
-  function HandelNo(){
-    setDeelet("")
+
+  function HandelNo() {
+    setDeelet("");
   }
 
   return (
     <body>
-      <section className=" w-full bg-[#161618] h-screen flex justify-center items-center text-white">
-        <div className=" content w-[80%] bg-zinc-900 text-center p-5 rounded-lg  border-solid border-2 border-teal-700">
-          <div className=" flex justify-between items-center border-solid border-2 border-teal-700  bg-zinc-900">
+      <section className="w-full bg-[#161618] h-screen p-5 text-white">
+        <div className="parent w-[80%] mt-20 m-auto">
+          <div className="flex justify-between items-center mb-7 border-solid border-2 border-teal-700 rounded-sm bg-zinc-900">
             <input
-              className=" w-[100%] bg-transparent p-2 "
+              className="w-[100%] bg-transparent p-2"
               type="text"
               placeholder="Add Your Task"
               value={input}
               onChange={(e) => {
                 setinput(e.target.value);
               }}
+              autoFocus
             />
             <div>
               <i
-                className=" fa-solid fa-plus font-bold text-2xl p-2 text-teal-700 "
+                className="fa-solid fa-plus font-bold text-2xl p-2 text-teal-700"
                 onClick={HandelAdd}
               ></i>
             </div>
           </div>
-          <div>{device}</div>
+          <div className="content bg-zinc-900  p-5 rounded- border-solid border-2 border-teal-700">
+            <div>{device}</div>
+          </div>
         </div>
       </section>
       <div>{Deelet}</div>
     </body>
   );
 }
-//<i class="fa-regular fa-circle-check"></i>
-//<i class="fa-regular fa-circle"></i>
