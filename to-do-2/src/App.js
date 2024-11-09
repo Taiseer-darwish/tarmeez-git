cd1d46fec06da038d067f59681628a0797b57fff
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-11-09 04:45:45 +0200

    to do 2

---
+++ to-do-2/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import Test from "./test";
+
+
+function App() {
+  return <>
+ <div className=" bg-[#161618] h-screen">
+ <Test/>
+ </div>
+  </>;
+}
+
+export default App;
+
+
+        // {/*TEXT */}
+        // <h1 className=" text-center text-5xl my-7 text-teal-800 font-bold">
+        //   To-Do App
+        // </h1>
+
+        // {/*The-Input & Button */}
+        // <div className="The-Input flex justify-between items-center mb-7 border-solid border-2 border-teal-700 rounded-sm bg-zinc-900">
+        //   <input
+        //     className="w-[100%] bg-transparent p-2"
+        //     type="text"
+        //     placeholder="Add Your Task"
+        //     value={input}
+        //     onChange={(e) => {
+        //       setinput(e.target.value);
+        //     }}
+        //     autoFocus
+        //   />
+        //   <div>
+        //     <i
+        //       className="fa-solid fa-plus font-bold text-2xl p-2 text-teal-700"
+        //       onClick={HandelAdd}
+        //     ></i>
+        //   </div>
+        // </div>
+
+        // {/*The-Output */}
+        // <div className="The-Output flex flex-col justify-center items-center w-full bg-zinc-900  p-5 rounded- border-solid border-2 border-teal-700">
+        //   {/*<Buttons */}
+        //   <Stack spacing={1} direction="row">
+        //     {/*<Button variant="contained">Contained</Button>*/}
+        //     <Button variant="outlined">ALL (0)</Button>
+        //     <Button variant="outlined">Done (0)</Button>
+        //     <Button variant="outlined">Outlined</Button>
+        //   </Stack>
+
+        //   {/*device */}
+        //   <div className="w-full ">{device}</div>
+        // </div>
+
+        // {/*Deelet Page */}
+        // <div>{Deelet}</div>
